5e64075f1939e5b9d124feeed08b041743ffedcc
HernanAC <39239369+HernanAC@users.noreply.github.com>
2023-03-02 07:20:13 -0500

    Adding new components
    Header, footer, content.

--- mercadolibre/src/App.js
+++ mercadolibre/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './views/Header.js';
+import Footer from './views/Footer.js';
+import Content from './views/Content.js';
+import ReactDOM from 'react-dom'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main >
+      <Header/>
+      <h1>Hola</h1>
+      <Footer/>
+    </main>
+    </>
   );
 }
 
---
+++ mercadolibre/src/views/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'; 
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+      header
+      </h1>
+    </header>
+  )
+}
+
+export default Header
+
