f65c6d9d683e5873652f91bb6674e45f631da5c7
HernanAC <39239369+HernanAC@users.noreply.github.com>
2023-03-08 17:03:07 -0500

    Changes on header and content
    Adding components

--- mercadolibre/src/views/Header.js
+++ mercadolibre/src/views/Header.js
@@ -1,13 +1,45 @@
 import React from 'react'
-import ReactDOM from 'react-dom'; 
+import "./header.css";
 
 
 const Header = () => {
   return (
-    <header>
-      <h1>
-      header
-      </h1>
+    <header class="header">
+      <div class="header">
+        <div>
+          <form class="form">
+            <div class="divform">
+              <div class="divform2" >
+                <div class="textoform">Buscar productos, marcas y más...
+                </div>
+              </div>
+            </div>
+            <div class="div2form">
+              <div class="pseudo">
+              </div>
+            </div>
+          </form>
+          <div class="domicilio">
+            Ingresa tu domicilio
+          </div>
+          <div class="categoria">
+            <lh>Categorías</lh>
+            <lh>Ofertas</lh>
+            <lh>Historial</lh>
+            <lh>Supermercado</lh>
+            <lh>Moda</lh>
+            <lh>Vender</lh>
+            <lh>Ayuda / PQR</lh>
+          </div>
+          <div class="cuenta">
+            <lh>Crea tu cuenta</lh>
+            <lh>Ingresa</lh>
+            <lh>Mis compras</lh>
+          </div>
+          <div class="logo">
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
